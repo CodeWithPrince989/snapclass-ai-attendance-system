8f2d21e00eaaa140268b2fd7dfa779d9d2e7be59
CodeWithPrince989 <codingwithprince01659@gmail.com>
2026-06-11 01:25:12 +0530

    Finished Project

--- src/database/config.py
+++ src/database/config.py
@@ -1,7 +1,9 @@
-import os
-from supabase import create_client
+import streamlit as st
 
-supabase = create_client(
-    os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_KEY")
+
+from supabase import create_client, Client
+
+supabase: Client = create_client(
+    st.secrets["SUPABASE_URL"],
+    st.secrets["SUPABASE_KEY"]
 )
